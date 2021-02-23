cba0d3f04e8ceafe206210cac4235b02a59cef9e
emptyewer <vkrishnamani@cercacor.com>
2021-02-22 18:04:32 -0800

    moved from qmake to cmake for all platforms + deployment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,34 @@
 source /etc/environment
-cd /builds/emptyewer/deepn-plus/
 
-qmake deepn.pro
-make -j8
-mkdir -p deepn++_linux_64bit
-mv main.o mainwindow.o moc_* ui_* Makefile DEEPN++ deepn++_linux_64bit/
+# ROOT_DIR="/builds/emptyewer/deepn-plus/libs"
+# # Compile Install Linux dependencies
+# BUILD_DIR=build_linux
+# for d in bpp-core bpp-seq bpp-seq-omics
+# do
+#     echo $ROOT_DIR"/"$d
+#     rm -rf $ROOT_DIR"/"$d"/"$BUILD_DIR
+#     mkdir -p $BUILD_DIR
+#     cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR"/"$d -B $ROOT_DIR"/"$d"/"$BUILD_DIR -DCMAKE_INSTALL_PREFIX=$BPP_DIR -DBUILD_STATIC=TRUE
+#     make -j2 -C $ROOT_DIR"/"$d"/"$BUILD_DIR
+# done
 
-/usr/lib/mxe/usr/x86_64-w64-mingw32.static/qt5/bin/qmake deepn.pro
-make -j8
-rm -rf Makefile* deepn++_plugin_import.cpp ui_mainwindow.h debug
-mv release deepn++_windows_64bit
+# # Compile Install Windows dependencies
+# BUILD_DIR=build_windows
+# for d in bpp-core bpp-seq bpp-seq-omics
+# do
+#     echo $ROOT_DIR"/"$d
+#     rm -rf $ROOT_DIR"/"$d"/"$BUILD_DIR
+#     mkdir -p $BUILD_DIR
+#     /usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR"/"$d -B $ROOT_DIR"/"$d"/"$BUILD_DIR -DCMAKE_INSTALL_PREFIX=$BPP_DIR -DBUILD_STATIC=TRUE
+#     make -j2 -C $ROOT_DIR"/"$d"/"$BUILD_DIR
+# done
 
-7z a deepn++_linux_64bit.7z deepn++_linux_64bit/*
-7z a deepn++_windows_64bit.7z deepn++_windows_64bit/*
+# Compile and build DEEPN Linux
+ROOT_DIR=/builds/emptyewer/deepn-plus
+cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_linux_release
+make -j2 -C $ROOT_DIR/build_linux_release
+7z a $ROOT_DIR/deepn++_linux_64bit.7z $ROOT_DIR/build_linux_release/DEEPN++
+
+/usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_windows_release
+make -C $ROOT_DIR/build_windows_release
+7z a $ROOT_DIR/deepn++_windows_64bit.7z $ROOT_DIR/build_windows_release/DEEPN++.exe
