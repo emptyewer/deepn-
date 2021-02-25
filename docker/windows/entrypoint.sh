aa7ef38aad9405d2cefbbfbe96354809bee0767b
emptyewer <vkrishnamani@cercacor.com>
2021-02-25 13:02:42 -0800

    split docker images for windows and linux builds

---
+++ docker/windows/entrypoint.sh
@@ -0,0 +1,11 @@
+source /etc/environment
+
+ROOT_DIR=/builds/emptyewer/deepn-plus
+# Compile and build DEEPN Windows
+rm -rf $ROOT_DIR/build_windows_release && mkdir -p $ROOT_DIR/build_windows_release
+cd $ROOT_DIR/build_windows_release
+conan install -pr:h windows .. -s build_type=Release
+cd ..
+/usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_windows_release
+make -j6 -C $ROOT_DIR/build_windows_release
+7z a -m0=ppmd -mx=9 $ROOT_DIR/deepn++_windows_64bit.7z $ROOT_DIR/build_windows_release/DEEPN++.exe
