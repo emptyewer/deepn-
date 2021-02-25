2187dbdfd8a607932fedf6bd46ea5c141cf3188d
emptyewer <vkrishnamani@cercacor.com>
2021-02-24 18:02:55 -0800

    static builds for QT for linux and windows, conan package manager support

--- compile_win_linux.sh
+++ compile_win_linux.sh
@@ -1,16 +1,1 @@
-# Download all the git
-
-
-# Compile Install Linux dependencies
-ROOT_DIR="/builds/emptyewer/deepn-plus/libs"
-BUILD_DIR=build_linux
-for d in bpp-core bpp-seq bpp-seq-omics
-do
-    echo $ROOT_DIR"/"$d
-    rm -rf $ROOT_DIR"/"$d"/"$BUILD_DIR
-    mkdir -p $BUILD_DIR
-    cmake -S $ROOT_DIR"/"$d -B $ROOT_DIR"/"$d"/"$BUILD_DIR -DCMAKE_INSTALL_PREFIX=$BPP_DIR -DBUILD_STATIC=TRUE
-    make -j8 -C $ROOT_DIR"/"$d"/"$BUILD_DIR
-done
-
-# docker run --rm -v $(pwd):/builds/emptyewer/deepn-plus/ venkykrishna/deepn:latest
+docker run --rm -v $(pwd):/builds/emptyewer/deepn-plus/ venkykrishna/deepn:latest
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,34 +1,20 @@
 source /etc/environment
 
-# ROOT_DIR="/builds/emptyewer/deepn-plus/libs"
-# # Compile Install Linux dependencies
-# BUILD_DIR=build_linux
-# for d in bpp-core bpp-seq bpp-seq-omics
-# do
-#     echo $ROOT_DIR"/"$d
-#     rm -rf $ROOT_DIR"/"$d"/"$BUILD_DIR
-#     mkdir -p $BUILD_DIR
-#     cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR"/"$d -B $ROOT_DIR"/"$d"/"$BUILD_DIR -DCMAKE_INSTALL_PREFIX=$BPP_DIR -DBUILD_STATIC=TRUE
-#     make -j2 -C $ROOT_DIR"/"$d"/"$BUILD_DIR
-# done
-
-# # Compile Install Windows dependencies
-# BUILD_DIR=build_windows
-# for d in bpp-core bpp-seq bpp-seq-omics
-# do
-#     echo $ROOT_DIR"/"$d
-#     rm -rf $ROOT_DIR"/"$d"/"$BUILD_DIR
-#     mkdir -p $BUILD_DIR
-#     /usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR"/"$d -B $ROOT_DIR"/"$d"/"$BUILD_DIR -DCMAKE_INSTALL_PREFIX=$BPP_DIR -DBUILD_STATIC=TRUE
-#     make -j2 -C $ROOT_DIR"/"$d"/"$BUILD_DIR
-# done
-
-# Compile and build DEEPN Linux
 ROOT_DIR=/builds/emptyewer/deepn-plus
-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_linux_release
-make -j2 -C $ROOT_DIR/build_linux_release
-7z a $ROOT_DIR/deepn++_linux_64bit.7z $ROOT_DIR/build_linux_release/DEEPN++
+# Compile and build DEEPN Linux
+rm -rf $ROOT_DIR/build_linux_release && mkdir -p $ROOT_DIR/build_linux_release
+cd $ROOT_DIR/build_linux_release
+conan install -pr:h linux .. -s build_type=Release
+cd ..
+cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_linux_release 
+make -j6 -C $ROOT_DIR/build_linux_release
+7z a -m0=ppmd -mx=9 $ROOT_DIR/deepn++_linux_64bit.7z $ROOT_DIR/build_linux_release/DEEPN++
 
+# Compile and build DEEPN Windows
+rm -rf $ROOT_DIR/build_windows_release && mkdir -p $ROOT_DIR/build_windows_release
+cd $ROOT_DIR/build_windows_release
+conan install -pr:h windows .. -s build_type=Release
+cd ..
 /usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_windows_release
-make -C $ROOT_DIR/build_windows_release
-7z a $ROOT_DIR/deepn++_windows_64bit.7z $ROOT_DIR/build_windows_release/DEEPN++.exe
+make -j6 -C $ROOT_DIR/build_windows_release
+7z a -m0=ppmd -mx=9 $ROOT_DIR/deepn++_windows_64bit.7z $ROOT_DIR/build_windows_release/DEEPN++.exe
