fd696c99a1285013a981051f19036de6ec635dad
emptyewer <vkrishnamani@cercacor.com>
2021-02-26 15:01:59 -0800

    adding quiet to docker build outputs, linux and windows launch of submodules

--- deepn/mainwindow.cpp
+++ deepn/mainwindow.cpp
@@ -1,5 +1,5 @@
-#include<QDebug>
-
+#include <QDebug>
+#include <QSysInfo>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -23,9 +23,17 @@ QString appendPath(const QString& path1, const QString& path2)
 void MainWindow::on_gene_count_btn_clicked()
 {
     QDir application_directory = QDir(QCoreApplication::applicationDirPath());
-    application_directory.cdUp();
-    application_directory.cdUp();
-    QString gene_count_path = appendPath(application_directory.path(), "Contents/Apps/GeneCount++.app/Contents/MacOS/GeneCount++");
+    QString gene_count_path;
+    if (QSysInfo::productType() == "osx" || QSysInfo::productType() == "macos") {
+        application_directory.cdUp();
+        application_directory.cdUp();
+        gene_count_path = appendPath(application_directory.path(), "Contents/Apps/GeneCount++.app/Contents/MacOS/GeneCount++");
+
+    } else if (QSysInfo::productType() == "windows" || QSysInfo::productType() == "winrt") {
+        gene_count_path = appendPath(application_directory.path(), "gene_count/GeneCount++.exe");
+    } else {
+        gene_count_path = appendPath(application_directory.path(), "gene_count/GeneCount++");
+    }
     QProcess::execute(QDir::toNativeSeparators(gene_count_path));
 }
 
@@ -33,8 +41,15 @@ void MainWindow::on_gene_count_btn_clicked()
 void MainWindow::on_junction_make_btn_clicked()
 {
     QDir application_directory = QDir(QCoreApplication::applicationDirPath());
-    application_directory.cdUp();
-    application_directory.cdUp();
-    QString junction_make_path = appendPath(application_directory.path(), "/Contents/Apps/JunctionMake++.app/Contents/MacOS/JunctionMake++");
+    QString junction_make_path;
+    if (QSysInfo::productType() == "osx" || QSysInfo::productType() == "macos") {
+        application_directory.cdUp();
+        application_directory.cdUp();
+        junction_make_path = appendPath(application_directory.path(), "/Contents/Apps/JunctionMake++.app/Contents/MacOS/JunctionMake++");
+    } else if (QSysInfo::productType() == "windows" || QSysInfo::productType() == "winrt") {
+        junction_make_path = appendPath(application_directory.path(), "junction_make/JunctionMake++.exe");
+    } else {
+        junction_make_path = appendPath(application_directory.path(), "junction_make/JunctionMake++");
+    }
     QProcess::execute(QDir::toNativeSeparators(junction_make_path));
 }
