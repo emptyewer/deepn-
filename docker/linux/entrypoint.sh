6208a209a264571bfd8abee9036a3bb1bdeb9a70
emptyewer <vkrishnamani@cercacor.com>
2021-03-03 11:34:27 -0800

    qt compile split into 5 stages

--- docker/linux/entrypoint.sh
+++ docker/linux/entrypoint.sh
@@ -4,5 +4,5 @@ source /etc/environment
 ROOT_DIR=/builds/emptyewer/deepn-plus
 rm -rf $ROOT_DIR/build_linux_release && mkdir -p $ROOT_DIR/build_linux_release
 cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_linux_release
-make -j6 -C $ROOT_DIR/build_linux_release
+make -C $ROOT_DIR/build_linux_release
 7z a -m0=ppmd -mx=9 $ROOT_DIR/deepn++_linux_64bit.7z $ROOT_DIR/build_linux_release{/**,}/*++
--- docker/windows/entrypoint.sh
+++ docker/windows/entrypoint.sh
@@ -4,5 +4,5 @@ ROOT_DIR=/builds/emptyewer/deepn-plus
 # Compile and build DEEPN Windows
 rm -rf $ROOT_DIR/build_windows_release && mkdir -p $ROOT_DIR/build_windows_release
 /usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_windows_release
-make -j6 -C $ROOT_DIR/build_windows_release
+make -C $ROOT_DIR/build_windows_release
 7z a -m0=ppmd -mx=9 $ROOT_DIR/deepn++_windows_64bit.7z $ROOT_DIR/build_windows_release{/**,}/*++.exe
