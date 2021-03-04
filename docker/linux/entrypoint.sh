77be3f9427de18cd0047c92d3fe5a4f56aef8523
emptyewer <vkrishnamani@cercacor.com>
2021-03-04 13:05:40 -0800

    added CI project dir var in docker

--- docker/linux/entrypoint.sh
+++ docker/linux/entrypoint.sh
@@ -1,8 +1,7 @@
 source /etc/environment
 
 # Compile and build DEEPN Linux
-ROOT_DIR=/builds/emptyewer/deepn-plus
-rm -rf $ROOT_DIR/build_linux_release && mkdir -p $ROOT_DIR/build_linux_release
-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_linux_release
-make -C $ROOT_DIR/build_linux_release
-7z a -m0=ppmd -mx=9 $ROOT_DIR/deepn++_linux_64bit.7z $ROOT_DIR/build_linux_release{/**,}/*++
+rm -rf $CI_PROJECT_DIR/build_linux_release && mkdir -p $CI_PROJECT_DIR/build_linux_release
+cmake -DCMAKE_BUILD_TYPE=Release -S $CI_PROJECT_DIR -B $CI_PROJECT_DIR/build_linux_release
+make -C $CI_PROJECT_DIR/build_linux_release
+7z a -m0=ppmd -mx=9 $CI_PROJECT_DIR/deepn++_linux_64bit.7z $CI_PROJECT_DIR/build_linux_release{/**,}/*++
