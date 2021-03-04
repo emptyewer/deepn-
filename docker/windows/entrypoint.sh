e754b3ea06b2a77ada661b8d19d79c7ef9602840
emptyewer <vkrishnamani@cercacor.com>
2021-03-04 14:10:28 -0800

    removed CI variable from compile

--- docker/windows/entrypoint.sh
+++ docker/windows/entrypoint.sh
@@ -1,7 +1,8 @@
 source /etc/environment
 
+ROOT_DIR=/builds/emptyewer/deepn-plus
 # Compile and build DEEPN Windows
-rm -rf $CI_PROJECT_DIR/build_windows_release && mkdir -p $CI_PROJECT_DIR/build_windows_release
-/usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $CI_PROJECT_DIR -B $CI_PROJECT_DIR/build_windows_release
-make -C $CI_PROJECT_DIR/build_windows_release
-7z a -m0=ppmd -mx=9 $CI_PROJECT_DIR/deepn++_windows_64bit.7z $CI_PROJECT_DIR/build_windows_release{/**,}/*++.exe
+rm -rf $ROOT_DIR/build_windows_release && mkdir -p $ROOT_DIR/build_windows_release
+/usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_windows_release
+make -C $ROOT_DIR/build_windows_release
+7z a -m0=ppmd -mx=9 $ROOT_DIR/deepn++_windows_64bit.7z $ROOT_DIR/build_windows_release{/**,}/*++.exe
