d4679c28cd9929e5ce816786f6b4f0608f05af40
emptyewer <vkrishnamani@cercacor.com>
2021-03-05 10:05:12 -0800

    changed mxe binary path in windows entrypoint

--- docker/windows/entrypoint.sh
+++ docker/windows/entrypoint.sh
@@ -3,6 +3,6 @@ source /etc/environment
 ROOT_DIR=/builds/emptyewer/deepn-plus
 # Compile and build DEEPN Windows
 rm -rf $ROOT_DIR/build_windows_release && mkdir -p $ROOT_DIR/build_windows_release
-/usr/lib/mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_windows_release
+mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_windows_release
 make -C $ROOT_DIR/build_windows_release
 7z a -m0=ppmd -mx=9 $ROOT_DIR/deepn++_windows_64bit.7z $ROOT_DIR/build_windows_release{/**,}/*++.exe
