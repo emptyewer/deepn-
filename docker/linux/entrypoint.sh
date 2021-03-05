b18ac0886db1b20754be9165bbaa5024d9f4efca
emptyewer <vkrishnamani@cercacor.com>
2021-03-05 15:04:12 -0800

    check if image exists before removing

--- docker/linux/entrypoint.sh
+++ docker/linux/entrypoint.sh
@@ -1,6 +1,6 @@
 source /etc/environment
 
-# Compile and build DEEPN Linux
+# Compile and build DEEPN Linux 
 ROOT_DIR=/builds/emptyewer/deepn-plus
 rm -rf $ROOT_DIR/build_linux_release && mkdir -p $ROOT_DIR/build_linux_release
 cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_linux_release
--- docker/windows/entrypoint.sh
+++ docker/windows/entrypoint.sh
@@ -1,7 +1,7 @@
 source /etc/environment
 
 ROOT_DIR=/builds/emptyewer/deepn-plus
-# Compile and build DEEPN Windows
+# Compile and build DEEPN Windows 
 rm -rf $ROOT_DIR/build_windows_release && mkdir -p $ROOT_DIR/build_windows_release
 /mxe/usr/bin/x86_64-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -S $ROOT_DIR -B $ROOT_DIR/build_windows_release
 make -C $ROOT_DIR/build_windows_release
