e7f2a73a22216bb53c42bae3d2370615335f3080
emptyewer <vkrishnamani@cercacor.com>
2021-03-20 14:46:26 -0700

    test commit

--- deepn/mainwindow.cpp
+++ deepn/mainwindow.cpp
@@ -1,5 +1,5 @@
 #include <QDebug>
-#include <plog/Log.h>
+//#include <plog/Log.h>
 #include <QSysInfo>
 #include <QCoreApplication>
 #include "mainwindow.h"
--- junction_make/mainwindow.cpp
+++ junction_make/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <plog/Log.h>
+//#include <plog/Log.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
